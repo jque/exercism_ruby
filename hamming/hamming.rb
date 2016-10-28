7fd0f68f8290251b7c4f8779432aa052fe35a7cd
Jorge R. Queiruga Triñanes <jorge@info-nova.com>
2016-10-28 14:04:18 +0200

    Improved exercise 2

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,13 +2,29 @@ module BookKeeping
   VERSION = 3
 end
 
+# 
+# Original solution 
+# 
+# class Hamming
+#   def self.compute dna1="", dna2=""
+#     raise ArgumentError unless dna1.length == dna2.length
+#
+#     (0..(dna1.length-1)).inject(0) do |memo, index|
+#        memo += 1 if dna1[index] != dna2[index]
+#        memo
+#     end
+#   end
+# end
+
+# 
+# Improved solution
+# 
 class Hamming
   def self.compute dna1="", dna2=""
     raise ArgumentError unless dna1.length == dna2.length
 
-    (0..(dna1.length-1)).inject(0) do |memo, index|
-       memo += 1 if dna1[index] != dna2[index]
-       memo
+    (0..(dna1.length-1)).count do |index|
+      dna1[index] != dna2[index]
     end
   end
 end
